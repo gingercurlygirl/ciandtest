15ee3cf1a64e81cb662f5cdb6ceaf1a8fdc493e3
Ivana Lovric <ivana.lovric1314@gmail.com>
2025-05-16 20:59:33 +0200

    spring intializr

---
+++ src/main/java/com/example/ciandtest/CiandtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ciandtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiandtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiandtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ciandtest/CiandtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ciandtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiandtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
