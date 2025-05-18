75bcd3c2b4c58e8c09f0f0ab55a8a8b04d363da3
Ivana Lovric <ivana.lovric1314@gmail.com>
2025-05-18 13:17:21 +0200

    TodoUnitTest implemented

--- src/main/java/com/example/ciandtest/model/Todo.java
+++ src/main/java/com/example/ciandtest/model/Todo.java
@@ -64,4 +64,12 @@ public class Todo {
     public void setUser(User user) {
         this.user = user;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
